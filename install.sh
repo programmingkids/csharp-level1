e64183c2c65e2c78108ebd2ce8549c06e2c8ec05
EC2 Default User <ec2-user@ip-172-31-32-120.ap-northeast-1.compute.internal>
2019-10-10 12:25:59 +0000

    fix bash on install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo " -- .Net Core is installed -- "
 
 echo " -- Setting is starting -- "
 echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
-. ~/.bashrc
+source ~/.bashrc
 
 mkdir -p ~/environment/.c9/runners
 touch ~/environment/.c9/runners/.NETCore.run
@@ -18,10 +18,10 @@ cat << EOM >> ~/environment/.c9/runners/.NETCore.run
 {
   "script": [
     "dotnet clean --verbosity minimal",
-    "env StartupObject=$file_base_name dotnet run"
+    "env StartupObject=\$file_base_name dotnet run"
   ],
-  "info": "Building $file_name and running $file_base_name",
-  "working_dir" : "$file_path",
+  "info": "Building \$file_name and running \$file_base_name",
+  "working_dir" : "\$file_path",
   "selector": "source.cs",
   "trackId": "CSharp"
 }
