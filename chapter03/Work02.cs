b59505831e5e2cd8d2d6cc08639ed2f0a55ceacb
EC2 Default User <ec2-user@ip-172-31-32-120.ap-northeast-1.compute.internal>
2019-10-11 01:57:15 +0000

    fix chapter03 for master

--- chapter03/Work02.cs
+++ chapter03/Work02.cs
@@ -3,12 +3,6 @@ using System;
 class Work02 {
     static void Main(string[] args)
     {
-        int a = 5;
-        float b = 2.5F;
-        float c = a + b;
-        int d = a + (int)b;
         
-        Console.WriteLine(c);
-        Console.WriteLine(d);
     }
 }
