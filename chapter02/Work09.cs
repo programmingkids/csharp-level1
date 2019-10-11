30de9e14eb94ca75ffe8de5f10610dba4abd8af5
EC2 Default User <ec2-user@ip-172-31-32-120.ap-northeast-1.compute.internal>
2019-10-11 01:24:29 +0000

    added chapter02

---
+++ chapter02/Work09.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Work09 {
+    static void Main(string[] args)
+    {
+        string name = "Minny";
+        int age = 17;
+        
+        Console.WriteLine(name + " is " + age.ToString() + " years old");
+    }
+}
---
+++ chapter02/Work10.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Work10 {
+    static void Main(string[] args)
+    {
+        string name = "Micky";
+        string hobby = "singing";
+        int hour = 7;
+        
+        Console.WriteLine(name + " loves " + hobby);
+        Console.WriteLine(name + " wakes up at " + hour.ToString() + " every day");
+    }
+}
