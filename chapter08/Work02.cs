b8fe45cc2fb94e30cbac279df7e7c85569ccf878
EC2 Default User <ec2-user@ip-172-31-32-120.ap-northeast-1.compute.internal>
2019-11-15 03:25:29 +0000

    fix chapter08 for master

--- chapter08/Work02.cs
+++ chapter08/Work02.cs
@@ -3,9 +3,6 @@ using System;
 class Work02 {
     static void Main(string[] args)
     {
-        Hero hero = new Hero("ドナルド",3, 12);
-        hero.attack();
         
-        // hero.getAttackPoint(); // このメソッドは実行できない
     }
 }
--- chapter08/Work04.cs
+++ chapter08/Work04.cs
@@ -3,10 +3,6 @@ using System;
 class Work04 {
     static void Main(string[] args)
     {
-        GameWorld gameWorld = GameWorld.getGameWorld();
-        gameWorld.run();
         
-        gameWorld = GameWorld.getGameWorld();
-        gameWorld.run();
     }
 }
